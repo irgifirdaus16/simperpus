c3d76f38296b9d1e9b87986e6e9ae1b86d5f05c1
irgifirdaus16 <ahmadirgifirdaus@gmail.com>
2023-06-04 21:02:58 +0700

    upload

---
+++ konfigurasi/config.php
@@ -0,0 +1,7 @@
+<?php
+//file config.php
+define('BASEPATH', 'http://localhost/simpustaka/');
+define('DBHOST', 'localhost:3307');
+define('DBUSER', 'root');
+define('DBPASS', '');
+define('DBNAME', 'simperpus');
